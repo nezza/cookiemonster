4806c29e9875424b8b00ae8b4a5f4863e7e83037
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2013-04-11 20:56:58 +0200

    Add cloudflare cookie

--- js/filters.js
+++ js/filters.js
@@ -109,6 +109,17 @@ function filter_check_if_expressionengine(cookie) {
 	return cookie;
 }
 
+function filter_check_if_cloudflare_id(cookie) {
+	if(cookie.name === "__cfduid") {
+		filter_obj = {}
+		filter_obj.name = "Cloudflare cookie";
+		filter_obj.shortname ="cloudflare";
+		filter_obj.description = "Cloudflare Cookie - see https://support.cloudflare.com/entries/22437336-What-does-the-CloudFlare-cfduid-cookie-do-";
+		cookie.filters.push(filter_obj);
+	}
+	return cookie;
+}
+
 filters = [
 	filter_check_if_rails_session, 
 	filter_check_if_django_session,
@@ -116,5 +127,6 @@ filters = [
 	filter_check_if_php_session,
 	filter_check_if_addthis,
 	filter_check_if_expressionengine,
+	filter_check_if_cloudflare_id,
 ]
 
